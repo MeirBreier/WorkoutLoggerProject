d738faaa892d4061bf5aa58b0303f541df25cbc0
UnnamedProject <205249755+MeirBreier@users.noreply.github.com>
2026-08-01 23:20:03 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE = 'log-v1';
+var ASSETS = ['./', './log.html', './manifest.json'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return Promise.all(ASSETS.map(function (u) {
+        return c.add(new Request(u, { cache: 'reload' })).catch(function () {});
+      }));
+    }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  var req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== self.location.origin) return;
+
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then(function (hit) {
+      var live = fetch(req).then(function (res) {
+        if (res && res.ok) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(req, copy); });
+        }
+        return res;
+      }).catch(function () { return hit; });
+      return hit || live;
+    }).catch(function () { return fetch(req); })
+  );
+});
